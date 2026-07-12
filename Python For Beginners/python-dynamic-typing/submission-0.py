53914a6ce523bbc4a59c10b4d95c8c293cad0e91
Kaushik Barman <kaushikbarman449@gmail.com>
2026-07-12 18:23:08 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.3
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Niki"
+print(type(variable))
+variable = [2, 3, "World"]
+print(type(variable))
