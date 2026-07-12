4b73cc4f1e96176cfc1b18e3b8b2114e16d8eb3f
Kaushik Barman <kaushikbarman449@gmail.com>
2026-07-13 01:27:35 +0530

    Add: python-dict-operations - submission-0

---
+++ Python For Beginners/python-dict-operations/submission-0.py
@@ -0,0 +1,14 @@
+your_dict = { 
+  "a": 10, 
+  "apple": 12,
+  "bat": 7
+}
+
+print(your_dict)
+print(your_dict["a"])
+
+print(True) if "d" in your_dict else print(False)
+
+
+your_dict["a"] = 4
+print(your_dict)
