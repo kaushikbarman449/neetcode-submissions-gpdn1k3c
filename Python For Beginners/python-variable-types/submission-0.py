a6154beeade2790b4d44e270fabf88efb25fbebf
Kaushik Barman <kaushikbarman449@gmail.com>
2026-07-12 15:22:56 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,10 @@
+integer_type = 10
+float_type = 3.4
+bool_type = False
+string_type = "Hello"
+list_type = ["Fifa", 4]
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
