e35d6306f02f589ae37ecf0a18da2ed57c56e24b
Kaushik Barman <kaushikbarman449@gmail.com>
2026-07-30 14:20:07 +0530

    Add: add-two-numbers - submission-0

---
+++ Data Structures & Algorithms/add-two-numbers/submission-0.py
@@ -0,0 +1,74 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+
+    def reverseList(self, head):
+        prev = None
+        current = head
+
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        
+        return prev
+
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        currentA = l1
+        currentB = l2
+        carry = 0
+
+        # resultant node
+        dummy = ListNode(0)
+        tail = dummy
+
+        while currentA or currentB or carry:
+            val1 = currentA.val if currentA else 0
+            val2 = currentB.val if currentB else 0
+
+            total_sum = val1 + val2 + carry
+
+            # store new digit
+            digit = total_sum % 10
+            carry = total_sum // 10
+
+            # append the digit to new list
+            tail.next = ListNode(digit)
+            tail = tail.next
+
+            if currentA:
+                currentA = currentA.next
+            
+            if currentB:
+                currentB = currentB.next
+
+        return dummy.next
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
