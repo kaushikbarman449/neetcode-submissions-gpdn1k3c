cd0dd4ebcb9b226405722dbcd01d20e8451726ff
Kaushik Barman <kaushikbarman449@gmail.com>
2026-08-23 04:44:22 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        n = len(nums)
+
+        for mask in range(1 << n):
+            subset = []
+            for i in range(n):
+                if mask & (1 << i):
+                    subset.append(nums[i])
+            
+            ans.append(subset)
+        return ans
